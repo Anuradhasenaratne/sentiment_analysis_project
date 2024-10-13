bb2783c131c7a30a7aef734ff7febd1fda6fdecd
Anuradha Senaratne <143251606+Anuradhasenaratne@users.noreply.github.com>
2024-10-13 14:03:08 +0530

    project done

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from helper import preprocessing, vectorizer, get_predticon
+import pandas as pd
 
 app = Flask(__name__)
 
+# Load tokens globally
+vocab = pd.read_csv('static/model/vocabulary.txt', header=None)
+tokens = vocab[0].tolist()
+
+# Global variables
 data = dict()
-rev = ['Good product', 'Bad product', 'I like it']
-positive = 2
-negative = 1
+rev = []
+positive = 0
+negative = 0
 
 @app.route("/")
 def index():
@@ -14,9 +21,30 @@ def index():
     data['negative'] = negative
     return render_template('index.html', data=data)
 
-@app.route("/" , methods = ['post'])
+@app.route("/", methods=['POST'])
 def my_post():
+    global positive, negative  # Declare global variables to modify them
+
     text = request.form['text']
+    preprocessed_txt = preprocessing(text)
+    vectorized_txt = vectorizer(preprocessed_txt, tokens)  # Pass tokens to vectorizer
+    prediction = get_predticon(vectorized_txt)
+
+    # Update counts based on prediction
+    if prediction == 'negative':
+        negative += 1
+    else:
+        positive += 1    
+
+    rev.insert(0, text)  # Add the new review to the beginning of the list
+
+    # Update the data dictionary with the new values
+    data['rev'] = rev
+    data['positive'] = positive
+    data['negative'] = negative
+
+    # Return the updated data to the template
+    return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- helper.py
+++ helper.py
@@ -3,49 +3,61 @@ import pandas as pd
 import re
 import string
 import pickle
-
 from nltk.stem import PorterStemmer
-ps= PorterStemmer()
 
-with open('static/model.pickle', 'rb')as f:
+# Initialize PorterStemmer
+ps = PorterStemmer()
+
+# Load the model and stopwords
+with open('static/model.pickle', 'rb') as f:
     model = pickle.load(f)
 
 with open('static/model/corpora/stopwords/english', 'r') as file:
- sw=file.read().splitlines()
+    sw = file.read().splitlines()
 
+# Load vocabulary
 vocab = pd.read_csv('static/model/vocabulary.txt', header=None)
 tokens = vocab[0].tolist()
 
 def remove_punctuations(text):
     for punctuation in string.punctuation:
-        text = text.replace(punctuation, '')  # Directly remove punctuation without adding space
+        text = text.replace(punctuation, '')  # Remove punctuation
     return text
 
 def preprocessing(text):
-    data =pd.DataFrame([text], columns=['tweet'])
-    data["tweet"]= data["tweet"].apply(lambda x: " ".join(ps.stem(x)for x in x.split()))
-    data["tweet"] = data["tweet"].apply(lambda x: " ".join(x.lower() for x in x.split()))
-    data["tweet"] = data['tweet'].apply(lambda x: " ".join(re.sub(r'https?:\/\/.*[\r\n]*', '', x, flags=re.MULTILINE) for x in x.split()))
+    data = pd.DataFrame([text], columns=['tweet'])
+    data["tweet"] = data["tweet"].apply(lambda x: " ".join(ps.stem(word) for word in x.split()))
+    data["tweet"] = data["tweet"].apply(lambda x: " ".join(word.lower() for word in x.split()))
+    data["tweet"] = data["tweet"].apply(lambda x: " ".join(re.sub(r'https?:\/\/.*[\r\n]*', '', x, flags=re.MULTILINE) for x in x.split()))
     data["tweet"] = data["tweet"].str.replace(r'\d+', '', regex=True)
     data["tweet"] = data["tweet"].apply(remove_punctuations)
-    data["tweet"]= data["tweet"].apply(lambda x: " ".join(x for x in x.split() if x not in sw))
-    return data ["tweet"]
+    data["tweet"] = data["tweet"].apply(lambda x: " ".join(word for word in x.split() if word not in sw))
+    
+    # Return the preprocessed text
+    return data["tweet"]
 
-
-def vectorizer(ds, vocabulary):
+def vectorizer(ds, tokens):
     vectorized_1st = []
-
+    
     for sentence in ds:
-        sentence_1st = np.zeros(len(vocabulary))
-
-        for i in range(len(vocabulary)):
-            if vocabulary[i] in sentence.split():
-                 sentence_1st[i] = 1
-                
-        vectorized_1st.append(sentence_1st)
+        sentence_1st = np.zeros(len(tokens))
         
+        for i in range(len(tokens)):
+            if tokens[i] in sentence.split():
+                sentence_1st[i] = 1
+        
+        vectorized_1st.append(sentence_1st)
+    
+    # Convert the list to a NumPy array
     vectorized_1st_new = np.asarray(vectorized_1st, dtype=np.float32)
-
+    
     return vectorized_1st_new
 
-def get_prediction()
+def get_predticon(vectorized_txt):
+    prediction = model.predict(vectorized_txt)
+    
+    # Assuming the model returns 0 for positive and 1 for negative
+    if prediction == 1:
+        return 'negative'
+    else:
+        return 'positive'
